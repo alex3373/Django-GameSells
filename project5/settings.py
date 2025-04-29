f6bdc894593e43ec5a4dbb7aa8a558bc23de8400
Alexx3319 <alexisandres522@gmail.com>
2025-04-29 03:15:21 -0400

    Update settings.py

--- project5/settings.py
+++ project5/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,11 +83,13 @@ WSGI_APPLICATION = 'project5.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:yuXRDOsmwdLBEkHnFPhGDSeMdGFHirNq@tramway.proxy.rlwy.net:26777/railway',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 REST_FRAMEWORK = { "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
     'DEFAULT_AUTHENTICATION_CLASSES': [
